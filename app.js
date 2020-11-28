109378fb7c1db95710566031f7f7cd09b89a3ed2
htmlcssphpjs <63463168+htmlcssphpjs@users.noreply.github.com>
2020-11-28 18:16:46 +0300

    #6

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var app = require('express')();
-//var http = require('http').Server(app);
+var http = require('http').Server(app);
 //порт для heroku нужен 5000
-var port = 5000;
-const server = app.listen(port, function(){
-  console.log( 'Express server started on http://localhost:' +
-    port + '; press Ctrl-C to stop.' );
-});
-var io = require('socket.io')(server);
+var io = require('socket.io')(http);
 var users = 0,
     codeget = '',
     name = '';
@@ -113,4 +108,8 @@ app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
+});
+
+http.listen(port, function(){
+  console.log('listening on: ' + port);
 });
